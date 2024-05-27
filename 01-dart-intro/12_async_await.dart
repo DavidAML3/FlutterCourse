4ac3437bb9d6f3d64b476dd894387e2d4fdc89ca
DavidAML <david.mejia@stellarelements.com>
2024-05-27 15:11:53 -0500

    working with async and await

---
+++ 01-dart-intro/12_async_await.dart
@@ -0,0 +1,18 @@
+void main() async {
+
+  print('Starting Programm');
+  try {
+    final value = await get('https://Google.com');
+    print(value);
+  } catch(error) {
+    print('We have a error: $error');
+  }
+  print('Ending Programm');
+}
+
+Future<String> get(String url) async {
+  
+  await Future.delayed(const Duration(seconds: 1));
+  throw 'We got an error in your petition';
+  return 'We have a value for your petition.';
+}
