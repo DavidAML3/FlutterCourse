009a5edf1e93fc765efca5cb62f16a3111cc7ab4
DavidAML <david.mejia@stellarelements.com>
2024-05-27 15:03:18 -0500

    working with futures

---
+++ 01-dart-intro/11_futures.dart
@@ -0,0 +1,17 @@
+void main() {
+
+  print('Starting Programm');
+  get('https://Google.com')
+  .then((value) => print(value))
+  .catchError((error) {
+    print('Error: $error');
+  });
+  print('Ending Programm');
+}
+
+Future<String> get(String url) {
+  return Future.delayed(Duration(seconds: 1), () {
+    throw 'Get petition failure';
+    return 'Get petition response';
+  });
+}
