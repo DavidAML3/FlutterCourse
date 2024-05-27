ac3a0d7c68b146b15880604681860911eac43841
DavidAML <david.mejia@stellarelements.com>
2024-05-27 11:13:42 -0500

    first commit

---
+++ 01-dart-intro/01_hello_world.dart
@@ -0,0 +1,10 @@
+void main () {
+  // var myName = 'David';
+  // late final myName;
+  //String myName = 'David';
+  const myName = 'David';
+
+  print('Hola $myName');
+  print('Hola ${ myName.toUpperCase() }');
+  print('Hola ${ 1 + 1 }');
+}
