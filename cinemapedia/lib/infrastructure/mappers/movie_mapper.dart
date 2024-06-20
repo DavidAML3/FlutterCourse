253b0d44fb8c78c5d41abbc8685a5d3225bf645f
DavidAML <david.mejia@stellarelements.com>
2024-06-20 08:12:00 -0500

    Initial commit and first implementation of cinemapedia

--- cinemapedia/lib/infrastructure/mappers/movie_mapper.dart
+++ cinemapedia/lib/infrastructure/mappers/movie_mapper.dart
@@ -5,7 +5,7 @@ class MovieMapper {
   static Movie movieDBToEntity(MovieMovieDB moviedb) => Movie(
       adult: moviedb.adult,
       backdropPath: moviedb.backdropPath != ''
-          ? 'https:..image.tmdb.org/t/p/w500${moviedb.backdropPath}'
+          ? 'https://image.tmdb.org/t/p/w500${moviedb.backdropPath}'
           : 'https://acortar.link/FNNEoA',
       genreIds: moviedb.genreIds.map((e) => e.toString()).toList(),
       id: moviedb.id,
@@ -14,7 +14,7 @@ class MovieMapper {
       overview: moviedb.overview,
       popularity: moviedb.popularity,
       posterPath: moviedb.posterPath != ''
-          ? 'https:..image.tmdb.org/t/p/w500${moviedb.posterPath}'
+          ? 'https://image.tmdb.org/t/p/w500${moviedb.posterPath}'
           : 'no-poster',
       releaseDate: moviedb.releaseDate,
       title: moviedb.title,
